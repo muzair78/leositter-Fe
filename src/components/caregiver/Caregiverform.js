276204af2867f2201589a397b1b91397d71e922d
muzair78 <inoxentali120@gmail.com>
2023-10-13 22:50:14 -0400

    new

--- src/components/caregiver/Caregiverform.js
+++ src/components/caregiver/Caregiverform.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { RiAccountPinCircleFill } from "react-icons/ri";
 import "react-toastify/dist/ReactToastify.css";
 import { toast } from "react-toastify";
-import { Checkbox, Form, Input, Select, Space } from "antd";
+import { Checkbox, Col, Form, Input, Row, Select, Space } from "antd";
 import "./JoinNow.css";
 import { useNavigate } from "react-router-dom";
 
@@ -117,169 +117,185 @@ const Caregiverform = () => {
   return (
     <>
       <div className="parent-div-pet">
-        <div className="form-container">
-          <div
-            style={{
-              textAlign: "center",
-              fontSize: "4rem",
-              marginTop: "1rem",
-            }}
-          >
+        <Row justify={"space-between"}>
+          <Col lg={12} md={12} sm={10} xs={15}>
             {" "}
-            <RiAccountPinCircleFill style={{ color: " #ff7875" }} />
-            <div
-              style={{
-                fontSize: "1.5rem",
-                fontWeight: "bolder",
-                color: " #ff7875",
-              }}
-            >
-              Caregiver Signup
-            </div>
-          </div>
-          <Form
-            labelCol={{ span: 4 }}
-            wrapperCol={{ span: 14 }}
-            layout="horizontal"
-            style={{ maxWidth: 600 }}
-            className="form-child"
-          >
-            <Form.Item label="Name">
-              <Space>
-                <Form.Item
-                  name="name"
-                  noStyle
-                  rules={[{ required: true, message: "Name is required" }]}
-                >
-                  <Input
-                    style={{ width: "20rem" }}
-                    name="name"
-                    placeholder="Your Name"
-                    value={user.name}
-                    onChange={userData}
-                  />
-                </Form.Item>
-              </Space>
-            </Form.Item>
-            <Form.Item label="Name">
-              <Space>
-                <Form.Item
-                  name="username"
-                  noStyle
-                  rules={[{ required: true, message: "Username is required" }]}
-                >
-                  <Input
-                    style={{ width: "20rem" }}
-                    name="username"
-                    placeholder="Your Username"
-                    value={user.username}
-                    onChange={userData}
-                  />
-                </Form.Item>
-              </Space>
-            </Form.Item>
-            <Form.Item label="Phone">
-              <Space>
-                <Form.Item
-                  name="phone"
-                  noStyle
-                  rules={[{ required: true, message: "Phone is required" }]}
-                >
-                  <Input
-                    style={{ width: "20rem" }}
-                    name="phone"
-                    placeholder="your phone number"
-                    value={user.phone}
-                    onChange={userData}
-                  />
-                </Form.Item>
-              </Space>
-            </Form.Item>
-            <Form.Item label="Email">
-              <Space>
-                <Form.Item
-                  name="email"
-                  noStyle
-                  rules={[{ required: true, message: "Email is required" }]}
-                >
-                  <Input
-                    name="email"
-                    style={{ width: "20rem" }}
-                    placeholder="Enter Email"
-                    type="email"
-                    value={user.email}
-                    onChange={userData}
-                  />
-                </Form.Item>
-              </Space>
-            </Form.Item>
-            <Form.Item label="Service">
-              <Form.Item
-                name="Pservice" // Corrected the name attribute to a string
-                noStyle
-                rules={[{ required: true, message: "Service is required" }]}
-                value={user.Pservice}
-                onChange={(value) => setUser({ ...user, Pservice: value })}
+            <div className="form-container">
+              <div
+                style={{
+                  textAlign: "center",
+                  fontSize: "4rem",
+                  marginTop: "1rem",
+                }}
               >
-                <Select
-                  placeholder="Which service do you offer?"
-                  onChange={(value) => setUser({ ...user, Pservice: value })}
+                {" "}
+                <RiAccountPinCircleFill style={{ color: " #ff7875" }} />
+                <div
+                  style={{
+                    fontSize: "1.5rem",
+                    fontWeight: "bolder",
+                    color: " #ff7875",
+                  }}
                 >
-                  <Option value="Cat Sitter">Cat Sitter</Option>
-                  <Option value="Dog Sitter">Dog Sitter</Option>
-                  <Option value="Bird Sitter">Bird Sitter</Option>
-                </Select>
-              </Form.Item>
-            </Form.Item>
-            <Form.Item label="Password">
-              <Space>
-                <Form.Item
-                  name="password"
-                  noStyle
-                  rules={[{ required: true, message: "Password is required" }]}
-                >
-                  <Input
-                    name="password"
-                    value={user.password}
-                    style={{ width: "20rem" }}
-                    placeholder="Create a strong password"
-                    onChange={userData}
-                    autoComplete={"off"}
-                  />
+                  Caregiver Signup
+                </div>
+              </div>
+              <Form
+                labelCol={{ span: 4 }}
+                wrapperCol={{ span: 14 }}
+                layout="horizontal"
+                style={{ maxWidth: 600 }}
+                className="form-child"
+              >
+                <Form.Item label="Name">
+                  <Space>
+                    <Form.Item
+                      name="name"
+                      noStyle
+                      rules={[{ required: true, message: "Name is required" }]}
+                    >
+                      <Input
+                        style={{ width: "20rem" }}
+                        name="name"
+                        placeholder="Your Name"
+                        value={user.name}
+                        onChange={userData}
+                      />
+                    </Form.Item>
+                  </Space>
                 </Form.Item>
-              </Space>
-            </Form.Item>
-            <Form.Item label="C_Password">
-              <Space>
-                <Form.Item
-                  name="cpassword"
-                  noStyle
-                  rules={[{ required: true, message: "Password is required" }]}
-                >
-                  <Input
-                    name="cpassword"
-                    style={{ width: "20rem" }}
-                    placeholder="Confirm Password"
-                    value={user.cpassword}
-                    onChange={userData}
-                    autoComplete={"off"}
-                  />
+                <Form.Item label="Name">
+                  <Space>
+                    <Form.Item
+                      name="username"
+                      noStyle
+                      rules={[
+                        { required: true, message: "Username is required" },
+                      ]}
+                    >
+                      <Input
+                        style={{ width: "20rem" }}
+                        name="username"
+                        placeholder="Your Username"
+                        value={user.username}
+                        onChange={userData}
+                      />
+                    </Form.Item>
+                  </Space>
                 </Form.Item>
-              </Space>
-            </Form.Item>
-            <Form.Item name="agreement" valuePropName="checked">
-              <Checkbox>I have read the agreement</Checkbox>
-            </Form.Item>
-            <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-              <button className="submit-btn" onClick={fetchUser}>
-                Signup
-              </button>
-            </Form.Item>
-          </Form>
-        </div>
-        <div className="pet-img">
-          <img src={cat} className="cat-img" alt="abc" />
-        </div>
+                <Form.Item label="Phone">
+                  <Space>
+                    <Form.Item
+                      name="phone"
+                      noStyle
+                      rules={[{ required: true, message: "Phone is required" }]}
+                    >
+                      <Input
+                        style={{ width: "20rem" }}
+                        name="phone"
+                        placeholder="your phone number"
+                        value={user.phone}
+                        onChange={userData}
+                      />
+                    </Form.Item>
+                  </Space>
+                </Form.Item>
+                <Form.Item label="Email">
+                  <Space>
+                    <Form.Item
+                      name="email"
+                      noStyle
+                      rules={[{ required: true, message: "Email is required" }]}
+                    >
+                      <Input
+                        name="email"
+                        style={{ width: "20rem" }}
+                        placeholder="Enter Email"
+                        type="email"
+                        value={user.email}
+                        onChange={userData}
+                      />
+                    </Form.Item>
+                  </Space>
+                </Form.Item>
+                <Form.Item label="Service">
+                  <Form.Item
+                    name="Pservice" // Corrected the name attribute to a string
+                    noStyle
+                    rules={[{ required: true, message: "Service is required" }]}
+                    value={user.Pservice}
+                    onChange={(value) => setUser({ ...user, Pservice: value })}
+                  >
+                    <Select
+                      placeholder="Which service do you offer?"
+                      onChange={(value) =>
+                        setUser({ ...user, Pservice: value })
+                      }
+                    >
+                      <Option value="Cat Sitter">Cat Sitter</Option>
+                      <Option value="Dog Sitter">Dog Sitter</Option>
+                      <Option value="Bird Sitter">Bird Sitter</Option>
+                    </Select>
+                  </Form.Item>
+                </Form.Item>
+                <Form.Item label="Password">
+                  <Space>
+                    <Form.Item
+                      name="password"
+                      noStyle
+                      rules={[
+                        { required: true, message: "Password is required" },
+                      ]}
+                    >
+                      <Input
+                        name="password"
+                        value={user.password}
+                        style={{ width: "20rem" }}
+                        placeholder="Create a strong password"
+                        onChange={userData}
+                        autoComplete={"off"}
+                      />
+                    </Form.Item>
+                  </Space>
+                </Form.Item>
+                <Form.Item label="C_Password">
+                  <Space>
+                    <Form.Item
+                      name="cpassword"
+                      noStyle
+                      rules={[
+                        { required: true, message: "Password is required" },
+                      ]}
+                    >
+                      <Input
+                        name="cpassword"
+                        style={{ width: "20rem" }}
+                        placeholder="Confirm Password"
+                        value={user.cpassword}
+                        onChange={userData}
+                        autoComplete={"off"}
+                      />
+                    </Form.Item>
+                  </Space>
+                </Form.Item>
+                <Form.Item name="agreement" valuePropName="checked">
+                  <Checkbox>I have read the agreement</Checkbox>
+                </Form.Item>
+                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                  <button className="submit-btn" onClick={fetchUser}>
+                    Signup
+                  </button>
+                </Form.Item>
+              </Form>
+            </div>
+          </Col>
+          <Col lg={12} md={9} sm={10} xs={15}>
+            {" "}
+            <div className="pet-img">
+              <img src={cat} className="cat-img" alt="abc" />
+            </div>
+          </Col>
+        </Row>
       </div>
     </>
   );
--- src/screen/Caregiverpages/Giverform.js
+++ src/screen/Caregiverpages/Giverform.js
@@ -6,9 +6,7 @@ import Footer from "../../components/footer/Footer";
 const Giverform = () => {
   return (
     <>
-      <Navbar />
       <Caregiverform />
-      <Footer />
     </>
   );
 };
