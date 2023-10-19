de55924251a037ac25a0817f75e076ff7d1fbcc0
muzair78 <inoxentali120@gmail.com>
2023-10-19 00:04:47 -0400

    new

--- src/components/FindJob/FindJob.js
+++ src/components/FindJob/FindJob.js
@@ -2,40 +2,35 @@ import React from "react";
 import { Col, Row, Button } from "antd";
 import { NavLink } from "react-router-dom";
 import "./FindJob.css";
+import { Card } from "antd";
 
 const FindJob = () => {
   return (
     <>
-      <div className="cards">
+      <div className="container-full">
         {" "}
-        <Row>
-          <div className="care-giver">
-            <Col sm={24} xs={24}>
-              <div className="care-content">
-                <h3 style={{ fontSize: "1.5rem" }}>I need a caregiver</h3>
-                <p>Start your free search for care in your area.</p>
-                <NavLink to={"/caretaker-signup"}>
-                  {" "}
-                  <Button type="primary" danger>
-                    Get Started
-                  </Button>
-                </NavLink>
-              </div>
-            </Col>
-          </div>{" "}
-          <div className="job-seeker">
-            <Col sm={24} xs={24}>
-              <div className="job-content">
-                <h3 style={{ fontSize: "1.5rem" }}>I want a care job</h3>
-                <p>Create a profile and search for jobs.</p>
-                <NavLink to={"/caregiver-signup"}>
-                  <Button danger type="primary">
-                    Get Started
-                  </Button>
-                </NavLink>
-              </div>
-            </Col>
-          </div>
+        <Row justify={"space-between"}>
+          <Col lg={11} md={11} sm={10} xs={24}>
+            <Card title="I need a caregiver" bordered={false}>
+              <p> Start your free search for care in your area.</p>
+              <NavLink to={"/caretaker-signup"}>
+                {" "}
+                <Button danger type="primary">
+                  Get Started
+                </Button>
+              </NavLink>
+            </Card>
+          </Col>
+          <Col lg={11} md={11} sm={10} xs={24}>
+            <Card title="I want a care job" bordered={false} id="s-card">
+              <p>Create a profile and search for jobs.</p>
+              <NavLink to={"/caregiver-signup"}>
+                <Button danger type="primary">
+                  Get Started
+                </Button>
+              </NavLink>
+            </Card>
+          </Col>
         </Row>
       </div>
     </>
